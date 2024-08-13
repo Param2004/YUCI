427d0ab95b7cce39ba10b32d5dda3a7d3d5c7b9d
Param sharma <kaushik.param2004@gmail.com>
2024-08-13 15:41:18 +0530

    few changes and new pages added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,16 @@ import Forums from './pages/forums'
 import JoinUs from './pages/joinUs'
 import WhyUs from './pages/whyUs'
 import ContactUs from './pages/contactUs'
+import Bureaucrat from './pages/forums/bureaucrat'
+import Celebrity from './pages/forums/celebrity'
+import Entrepreneur from './pages/forums/entrepreneur'
+import GlobalEmbassy from './pages/forums/globalEmbassy'
+import InternationalEcinomic from './pages/forums/internationalEconomic'
+import Law from './pages/forums/law'
+import Medico from './pages/forums/medico'
+import Political from './pages/forums/political'
+import Sports from './pages/forums/sports'
+import Youth from './pages/forums/youth'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css'
 
@@ -18,13 +28,24 @@ export default function App() {
     <Router>
       <Header />
       <Routes>
+      
         <Route path="/" element={<Home />} />
-        <Route path="/forums" element={<Forums />} >
-          <Route path="/forums/bureaucrat-forum" element={<Forums />}></Route>
-        </Route>
+        <Route path="/forums" element={<Forums />} ></Route>
         <Route path="/join-us" element={<JoinUs />} />
         <Route path="/why-us" element={<WhyUs />} />
         <Route path="/contact-us" element={<ContactUs />} />
+
+        <Route path="/forums/bureaucrat-forum" element={<Bureaucrat />}></Route>
+        <Route path="/forums/celebrity-forum" element={<Celebrity />}></Route>
+        <Route path="/forums/entrepreneur-forum" element={<Entrepreneur />}></Route>
+        <Route path="/forums/global-embassy-forum" element={<GlobalEmbassy />}></Route>
+        <Route path="/forums/international-ecinomic-forum" element={<InternationalEcinomic />}></Route>
+        <Route path="/forums/law-forum" element={<Law />}></Route>
+        <Route path="/forums/medico-forum" element={<Medico />}></Route>
+        <Route path="/forums/political-forum" element={<Political />}></Route>
+        <Route path="/forums/sports-forum" element={<Sports />}></Route>
+        <Route path="/forums/youth-forum" element={<Youth />}></Route>
+
       </Routes>
       <Footer />
     </Router>
